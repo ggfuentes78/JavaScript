1d47f3abf09a996978d991208a6aaa7cdf6b6a93
Gustavo Fuentes <gg_fuentes78@hotmail.com>
2021-08-30 23:37:28 -0300

    Clase1

---
+++ Clase1/clase1.js
@@ -0,0 +1,4 @@
+const AÑOACTUAL = 2021
+let nombre = prompt("Ingrese su nombre")
+let añoNacimiento = parseInt(prompt("Ingrese el año de su nacimiento"))
+alert ("Hola " + nombre + "! Según mis cálculos este año cumplís "+ (AÑOACTUAL - añoNacimiento) + " años.")
